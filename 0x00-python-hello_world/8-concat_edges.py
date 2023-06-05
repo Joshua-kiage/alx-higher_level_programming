918a910dc35c0ce528c3095e6af129b9f5223937
root <Joshua-kiage>
2023-06-05 11:50:31 -0700

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,3 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming \
 language that combines remarkable power with very clear syntax"
 print(str[str.index("object-oriented"):str.index("object-oriented") + len("object-oriented")])
+print(str)
