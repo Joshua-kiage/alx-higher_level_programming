3504b94bd5036ccde28f14c08dc26153346e7358
root <Joshua-kiage>
2023-06-05 13:35:01 -0700

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,84 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * check_cycle - checks if a singly linked list has a cycle.
- * @list: pointer to the beginning of the node
- * Return: 0 if no cycle, 1 if there is a cycle
+ * print_listint - Prints all elements of a listint_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: Number of nodes in the list.
  */
-
-int check_cycle(listint_t *list)
+size_t print_listint(const listint_t *h)
 {
-	listint_t *crnt, *check;
+	const listint_t *current = h;
+	size_t n = 0;
 
-	if (list == NULL || list->nxt == NULL)
-		return (0);
-	crnt = list;
-	check = crnt->nxt;
-
-	while (crnt != NULL && check->nxt != NULL
-			&& check->nxt->nxt != NULL)
+	while (current != NULL)
 	{
-		if (crnt == check)
-			return (1);
-		crnt = crnt->nxt;
-		check = check->nxt->nxt;
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
 	}
+
+	return (n);
+}
+
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: Pointer to a pointer of the start of the list.
+ * @n: Integer to be included in the new node.
+ *
+ * Return: Address of the new element or NULL if it fails.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: Pointer to the head of the list to be freed.
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	listint_t *head = NULL;
+
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+
+	print_listint(head);
+
+	free_listint(head);
+
 	return (0);
 }
