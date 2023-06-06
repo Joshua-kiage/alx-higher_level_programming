9d00afbb3ce48a29bdf4510b17335351364e5751
Joshua-Kiage <joshkiage64@gmail.com>
2023-06-06 18:30:33 +0000

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for character in range(ord('a'), ord('z') + ord('1')):
-    print(chr(character), end='')
+for character in range(97, 123):
+    print("{:c}".format(character), end='')
