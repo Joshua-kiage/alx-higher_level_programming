fcba48f008490d9f1d1a24c02002079b2094a102
Joshua-Kiage <joshkiage64@gmail.com>
2023-06-08 17:04:12 +0000

    1. My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, subtract, multiply, divide
+
+a = 10
+b = 5
+
+sum_result = add(a, b)
+diff_result = subtract(a, b)
+prod_result = multiply(a, b)
+div_result = divide(a, b)
+
+print(f"Sum: {a} + {b} = {sum_result}")
+print(f"Difference: {a} - {b} = {diff_result}")
+print(f"Product: {a} * {b} = {prod_result}")
+print(f"Division: {a} / {b} = {div_result}")
