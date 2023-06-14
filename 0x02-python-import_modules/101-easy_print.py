d00f9525528c81b4956af81e75f8348b7cb1ac71
Joshua-Kiage <joshkiage64@gmail.com>
2023-06-14 18:30:05 +0000

    7. Easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-__builtins__.__dict__['_'*6].__call__(__builtins__.__dict__['_'*2]+__builtins__.__dict__['_'*0]+__builtins__.__dict__['_'*1])
+getattr(getattr(__builtins__, '__dict__', {}), '_'*6)('', '#pythoniscool')
