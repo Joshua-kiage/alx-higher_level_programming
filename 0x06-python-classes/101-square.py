82e3bc5af3b357fd6ca4ffbcd13d1b87f45cf5e0
Joshua-Kiage <joshkiage64@gmail.com>
2023-06-27 15:17:50 +0000

    8. Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -36,7 +36,7 @@ class Square:
         Returns:
             int: The size of the square.
         """
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -65,7 +65,7 @@ class Square:
         Returns:
             tuple: The position of the square.
         """
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -95,7 +95,7 @@ class Square:
         Returns:
             int: The area of the square.
         """
-        return (self.__size ** 2)
+        return self.__size ** 2
 
     def my_print(self):
         """
@@ -110,22 +110,3 @@ class Square:
                 print()
             for _ in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
-
-    def __str__(self):
-        """
-        Returns a string representation of the square.
-
-        Returns:
-            str: The string representation of the square.
-        """
-        square_str = ""
-        if self.__size == 0:
-            return square_str
-
-        for _ in range(self.__position[1]):
-            square_str += "\n"
-
-        for _ in range(self.__size):
-            square_str += " " * self.__position[0] + "#" * self.__size + "\n"
-
-        return (square_str[:-1])
