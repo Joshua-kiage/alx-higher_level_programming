7035d441f77b5abe01319b29275b2012681cd550
Joshua-Kiage <joshkiage64@gmail.com>
2023-06-27 15:12:45 +0000

    8. Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -95,7 +95,7 @@ class Square:
         Returns:
             int: The area of the square.
         """
-        return self.__size ** 2
+        return (self.__size ** 2)
 
     def my_print(self):
         """
@@ -110,3 +110,22 @@ class Square:
                 print()
             for _ in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: The string representation of the square.
+        """
+        square_str = ""
+        if self.__size == 0:
+            return square_str
+
+        for _ in range(self.__position[1]):
+            square_str += "\n"
+
+        for _ in range(self.__size):
+            square_str += " " * self.__position[0] + "#" * self.__size + "\n"
+
+        return (square_str[:-1])
